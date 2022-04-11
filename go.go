b3c64334d6feac68a97d5f6338bf7cdfe6674b10
Eric Yager <ejyager00@gmail.com>
2022-04-11 18:08:54 -0500

    refactor to improve code reusability

--- go.go
+++ go.go
@@ -15,6 +15,105 @@ type GoGame struct {
 	piecesCaptured    [2]int
 	komi              float32
 }
+type MoveResults struct {
+	err          error
+	victory      int8
+	turn         int8
+	currentBoard GoBoard
+	currentScore [2]float32
+}
+type GameArchive struct {
+	boardSize   int
+	komi        float32
+	winner      int8
+	moveHistory [][2]int
+	score       [2]float32
+}
+
+func StartCommandLineGo() {
+	fmt.Println("The board is x, y indexed from the top left corner starting with 0.")
+	fmt.Println("Input moves with two integers separated by a space representing x and y respectively.")
+	fmt.Println("To pass, input -1 and to resign input -2.")
+
+	var s int
+	var k float32
+	fmt.Print("Input board size and komi (white advantage) separated by a space: ")
+	fmt.Scanf("%d %f", &s, &k)
+
+	move_channel := make(chan [2]int)
+	defer close(move_channel)
+	results_channel := make(chan MoveResults)
+
+	go NewGameSession(s, k, move_channel, results_channel, nil)
+
+	gameover := false
+	move_results := <-results_channel
+	fmt.Println(BoardString(move_results.currentBoard))
+	for !gameover {
+		if move_results.turn == 1 {
+			fmt.Print("Enter black's move: ")
+		} else {
+			fmt.Print("Enter white's move: ")
+		}
+		var move_x, move_y int
+		fmt.Scanf("%d %d\n", &move_x, &move_y)
+
+		move_channel <- [2]int{move_x, move_y}
+		move_results = <-results_channel
+
+		fmt.Println(BoardString(move_results.currentBoard))
+		if move_results.err != nil {
+			fmt.Println(move_results.err)
+		}
+		if move_results.victory != 0 {
+			gameover = true
+			if move_results.victory > 0 {
+				fmt.Printf("Black wins, %.1f to %.1f\n", move_results.currentScore[0], move_results.currentScore[1])
+			} else {
+				fmt.Printf("White wins, %.1f to %.1f\n", move_results.currentScore[1], move_results.currentScore[0])
+			}
+		}
+	}
+}
+
+func NewGameSession(size int, komi float32, in_ch chan [2]int, out_ch chan MoveResults, final_ch chan GameArchive) {
+	game := NewGame(size, komi)
+	defer close(out_ch)
+	if final_ch != nil {
+		defer close(final_ch)
+	}
+	var err error
+	var victory int8
+	moves := make([][2]int, 0)
+	out_ch <- MoveResults{nil, 0, game.turn, game.board, [2]float32{0, komi}}
+	for move := range in_ch {
+		x := move[0]
+		y := move[1]
+		if x == -1 || y == -1 {
+			victory, err = game.MakeMove(0, 0, true, false)
+			if err == nil {
+				moves = append(moves, move)
+			}
+		} else if x == -2 || y == -2 {
+			victory, err = game.MakeMove(0, 0, false, true)
+			if err == nil {
+				moves = append(moves, move)
+			}
+		} else {
+			victory, err = game.MakeMove(x, y, false, false)
+			if err == nil {
+				moves = append(moves, move)
+			}
+		}
+		out_ch <- MoveResults{err, victory, game.turn, game.board, game.GetScores()}
+		if victory != 0 {
+			break
+		}
+	}
+	if final_ch != nil {
+		final_ch <- GameArchive{size, komi, victory, moves, game.GetScores()}
+	}
+}
 
 func NewGame(sideLength int, komi float32) GoGame {
 	game := GoGame{sideLength, newBoard(sideLength), nil, 1, 0, [2]int{0, 0}, komi}
@@ -34,20 +133,40 @@ func startHistory(board GoBoard) []string {
 	return []string{fmt.Sprint(board)}
 }
 
-func (game GoGame) PrintBoard() {
+func (game GoGame) BoardString() string {
+	board_string := ""
 	for i := range game.board {
 		for j := range game.board[i] {
 			if game.board[i][j] == 1 {
-				fmt.Print("●")
+				board_string += "●"
 			} else if game.board[i][j] == -1 {
-				fmt.Print("○")
+				board_string += "○"
 			} else {
-				fmt.Print("+")
+				board_string += "+"
 			}
 		}
-		fmt.Println("")
+		board_string += "\n"
 	}
-	fmt.Println("")
+	board_string += "\n"
+	return board_string
+}
+
+func BoardString(board GoBoard) string {
+	board_string := ""
+	for i := range board {
+		for j := range board[i] {
+			if board[i][j] == 1 {
+				board_string += "●"
+			} else if board[i][j] == -1 {
+				board_string += "○"
+			} else {
+				board_string += "+"
+			}
+		}
+		board_string += "\n"
+	}
+	board_string += "\n"
+	return board_string
 }
 
 func (game *GoGame) MakeMove(x, y int, pass, resign bool) (int8, error) {
@@ -70,8 +189,7 @@ func (game *GoGame) MakeMove(x, y int, pass, resign bool) (int8, error) {
 	if game.board[y][x] != 0 {
 		return 0, fmt.Errorf("there is already a piece at %d, %d", x, y)
 	}
-	boardCopy := make([][]int8, game.size)
-	copy(boardCopy, game.board)
+	boardCopy := copyBoard(game.board)
 	boardCopy[y][x] = game.turn
 	if y-1 >= 0 && boardCopy[y-1][x] == -1*game.turn {
 		friends := getConnectedFriends(boardCopy, x, y-1, game.size, [][2]int{{x, y - 1}})
@@ -139,7 +257,7 @@ func (game *GoGame) MakeMove(x, y int, pass, resign bool) (int8, error) {
 	if inHistory(boardCopy, &game.history) {
 		return 0, errors.New("this move violates the ko rule")
 	}
-	copy(game.board, boardCopy)
+	game.board = copyBoard(boardCopy)
 	game.turn *= -1
 	game.history = append(game.history, fmt.Sprint(game.board))
 	game.consecutivePasses = 0
@@ -151,8 +269,7 @@ func (game GoGame) GetScores() [2]float32 {
 	scores[1] += game.komi
 	scores[0] += float32(game.piecesCaptured[0])
 	scores[1] += float32(game.piecesCaptured[1])
-	boardCopy := make([][]int8, game.size)
-	copy(boardCopy, game.board)
+	boardCopy := copyBoard(game.board)
 	for i := 0; i < game.size; i++ {
 		for j := 0; j < game.size; j++ {
 			if boardCopy[i][j] == 0 {
@@ -265,4 +382,14 @@ func inHistory(board GoBoard, gameHistory *[]string) bool {
 		}
 	}
 	return false
+}
+
+func copyBoard(board GoBoard) GoBoard {
+	boardCopy := newBoard(len(board))
+	for i := range board {
+		for j := range board[i] {
+			boardCopy[i][j] = board[i][j]
+		}
+	}
+	return boardCopy
 }
--- main.go
+++ main.go
@@ -1,51 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("The board is x, y indexed from the top left corner starting with 0.")
-	fmt.Println("Input moves with two integers separated by a space representing x and y respectively.")
-	fmt.Println("To pass, input -1 and to resign input -2.")
-
-	var s int
-	var k float32
-	fmt.Print("Input board size and komi (white advantage) separated by a space: ")
-	fmt.Scanf("%d %f", &s, &k)
-
-	game := NewGame(s, k)
-
-	gameover := false
-	for !gameover {
-		game.PrintBoard()
-		if game.turn == 1 {
-			fmt.Print("Enter black's move: ")
-		} else {
-			fmt.Print("Enter white's move: ")
-		}
-		var move_x, move_y int
-		fmt.Scanf("%d %d\n", &move_x, &move_y)
-		var err error
-		var victory int8
-		if move_x == -1 {
-			victory, err = game.MakeMove(0, 0, true, false)
-		} else if move_x == -2 {
-			victory, err = game.MakeMove(0, 0, false, true)
-		} else {
-			victory, err = game.MakeMove(move_x, move_y, false, false)
-		}
-		if err != nil {
-			fmt.Println(err)
-		}
-		if victory != 0 {
-			gameover = true
-			scores := game.GetScores()
-			if victory > 0 {
-				fmt.Printf("Black wins, %.1f to %.1f\n", scores[0], scores[1])
-			} else {
-				fmt.Printf("White wins, %.1f to %.1f\n", scores[1], scores[0])
-			}
-		}
-	}
+	StartCommandLineGo()
 }
